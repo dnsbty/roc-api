f7f0c9285af6a54cc503ef54b79f24fbf31586cc
Dennis Beatty <dennis@dcbeatty.com>
2016-08-27 01:07:22 -0600

    new game form

--- routes/schedule.js
+++ routes/schedule.js
@@ -5,11 +5,15 @@ var path = require('path');
 
 /* GET schedule page */
 router.get('/', function(req, res, next) {
-	var content = fs.readFileSync(path.resolve(__dirname, '../public/json/schedule.json'));
-	var games = JSON.parse(content);
+	var games = require('../public/json/schedule.json');
 	res.render('schedule/index', {
 		games: games
 	});
+});
+
+/* GET new scheduled game page */
+router.get('/new', function(req, res, next) {
+	res.render('schedule/new');
 });
 
 module.exports = router;
