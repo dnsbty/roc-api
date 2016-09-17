52e4773d18a376ca18569cffbeeca2e8c89dd2d3
Dennis Beatty <dennis@dcbeatty.com>
2016-09-16 19:38:37 -0600

    allow cross domain traffic for phones

--- routes/phones.js
+++ routes/phones.js
@@ -3,9 +3,14 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 
+/* make sure we're allowing cross domain traffic */
+router.options('/', function(req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+});
+
 /* POST new phone number */
 router.post('/', function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', 'https://therocapp.com');
+	res.header("Access-Control-Allow-Origin", "*");
 
 	if (!req.body || !req.body.number || !req.body.type || req.body.number == '' || req.body.type == '') {
 		return res.status(400).json({ message: 'Missing required fields' });
